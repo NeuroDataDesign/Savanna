d7602ef2f813a289ad9ff4337bdb32acf6ebe3a6
bvarjavand <bvarjavand@gmail.com>
2019-09-23 18:03:46 -0400

    dataset tests pass now

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -3,5 +3,5 @@ from savanna.utils.dataset import *
 
 def test_get_dataset():
     trainset, testset = get_dataset("../savanna/data", "FashionMNIST", is_numpy=True)
-    assert isinstance(trainset, np.ndarray)
+    assert isinstance(trainset, tuple)
 
