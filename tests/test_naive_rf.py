24e4510b8679fb287e1afad855719b25fda58b1b
Apple <apple@Apples-MacBook-Pro.local>
2019-09-25 14:27:46 -0400

    prelim test

--- tests/test_naive_rf.py
+++ tests/test_naive_rf.py
@@ -1,14 +1,13 @@
 import pytest
-
-import time
-
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-
-from savanna.utils.dataset import get_subset_data
 from savanna.utils.utils import run_experiment
 from savanna.random_forest.naive_rf import run_naive_rf
+from savanna.random_forest.naive_rerf import run_naive_rerf
 
 def test_run_on_naive_rf():
     results = run_experiment(run_naive_rf, "Naive RF")
     assert isinstance(results, list)
+
+def test_naive_rerf():
+    results = run_experiment(run_naive_rerf, "Naive Rerf")
+    assert isinstance(results, list)
+    
