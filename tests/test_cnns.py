2eb47e6e688e3c9c3dd1c5f9fe19788b7528d7ca
AF1111 <afiallo1@jhu.edu>
2019-09-29 23:25:18 -0400

    Removed unneccesary lines

--- tests/test_cnns.py
+++ tests/test_cnns.py
@@ -1,14 +1,9 @@
 import pytest
 import copy
-#from argparse import ArgumentParser
 import torch
 from savanna.utils.utils import run_experiment
 from savanna.cnn.trainer import run_cnn
 from savanna.cnn.models.simple import SimpleCNN1layer, SimpleCNN2Layers
-from savanna.utils.deep_conv_rf_runners import run_one_layer_deep_conv_rf
-
-
-
 
 def test_1layer_cnn():
 
@@ -17,16 +12,15 @@ def test_1layer_cnn():
     DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
     CHOOSEN_CLASSES = [0,3]
     NUM_CLASSES = len(CHOOSEN_CLASSES)
-    IMG_SHAPE = (25,25,3)
+    IMG_SHAPE = (25,25,1)
 
     ### Make sure all dependencies are above this line
     CNN_CONFIG = {"batch_size": BATCH_SIZE, "epoch": EPOCH, "device": DEVICE}
 
     cnn_acc_vs_n_config = copy.deepcopy(CNN_CONFIG)
     cnn_acc_vs_n_config.update({'model': 0, 'lr': 0.001, 'weight_decay': 1e-05})
-    #results = run_experiment(run_cnn, "cnn_acc_vs_n", "CNN (1-layer, 1-filter)",
-    #               cnn_model=SimpleCNN1layer(1, NUM_CLASSES, IMG_SHAPE), cnn_config=cnn_acc_vs_n_config)
-    results = run_experiment(run_one_layer_deep_conv_rf,
-                   "deep_conv_rf_old_acc_vs_n", rf_type="unshared")
+    results = run_experiment(run_cnn,"CNN (1-layer, 1-filter)",
+                  cnn_model=SimpleCNN1layer(1, NUM_CLASSES, IMG_SHAPE), cnn_config=cnn_acc_vs_n_config)
+
 
     assert isinstance(results, list)
