1b1e329a90ef6b82ff08d02c4c66b2acd192983e
AF1111 <afiallo1@jhu.edu>
2019-10-09 03:01:29 -0400

    Added tests for 1,2 layer deep_conv_rf

---
+++ tests/test_deep_conv_rf.py
@@ -0,0 +1,24 @@
+import pytest
+from savanna.utils.deep_conv_rf_runners import (
+    run_one_layer_deep_conv_rf,
+    run_two_layer_deep_conv_rf,
+)
+from savanna.utils.utils import run_experiment
+
+
+def test_one_layer_deep_conv_rf():
+
+    results = run_experiment(
+        run_one_layer_deep_conv_rf, "deep_conv_rf_old_acc_vs_n", rf_type="unshared"
+    )
+
+    assert isinstance(results, list)
+
+
+def test_two_layer_deep_conv_rf():
+
+    results = run_experiment(
+        run_two_layer_deep_conv_rf, "deep_conv_rf_old_acc_vs_n", rf_type="unshared"
+    )
+
+    assert isinstance(results, list)
