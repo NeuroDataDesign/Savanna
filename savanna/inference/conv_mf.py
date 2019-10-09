4e55a16efc4e43af629762d1bddf2ed4afdf7357
ryanlu41 <ryanlu41@gmail.com>
2019-10-09 07:52:49 -0400

    Fleshed out tests for conv_mf, and modified a lot the functionality to make convmf make more sense

--- savanna/inference/conv_mf.py
+++ savanna/inference/conv_mf.py
@@ -7,40 +7,43 @@ from sklearn.ensemble import RandomForestClassifier
 from rerf.rerfClassifier import rerfClassifier
 
 class ConvMF(object):
-    def __init__(self, num_trees = 1000, tree_type = 'S-RerF'):
+    def __init__(self, type = 'native', num_trees = 1000, tree_type = 'S-RerF', patch_height_min = 1, patch_width_min = 1, patch_height_max = 5, patch_width_max = 5):
         self.num_trees = num_trees;
         self.tree_type = tree_type;
+        self.type = type;
+        self.patch_height_min = patch_height_min
+        self.patch_height_max = patch_height_max
+        self.patch_width_max = patch_width_max
+        self.patch_width_min = patch_width_min
         self.time_taken = {"load": 0, "test_chop": 0, "train": 0, "fit": 0, "train_post": 0, "test": 0, "test_post": 0}
 
     def fit(self, images, labels):
-        #Confused by this... ask Bijan
-        def approx_predict_proba_sample_wise(sample):
-            return np.array(self.forest.predict_proba(sample.tolist())[1] / float(self.num_trees))
+        MF_image = np.zeros(5)
+        if self.type == 'native':
+            batch_size, length, width,_ = images.shape
 
 
+            reshaped_images = images.reshape(batch_size, length*width)
 
 
-        batch_size, length, width,_ = images.shape
-        MF_image = np.zeros((images.shape[0], length, width, 1))
+            self.forest = rerfClassifier(projection_matrix="S-RerF",
+                                             n_estimators=self.num_trees,
+                                             n_jobs=cpu_count() - 1,
+                                             image_height=length,
+                                             image_width=width,
+                                             patch_height_min=self.patch_height_min,
+                                             patch_width_min=self.patch_width_min,
+                                             patch_height_max=self.patch_height_max,
+                                             patch_width_max=self.patch_height_min)
+            self.forest.fit(reshaped_images, labels)
+            #Is this necessary
+            #for i in range(length):
+            #    for j in range(width):
+            #        x = 1
+            #        MF_image[:, i, j] = np.array([approx_predict_proba_sample_wise(
+            #            sample) for sample in images[:, i, j]])[..., np.newaxis]
 
-        reshaped_images = images.reshape(batch_size, length*width)
-
-
-        self.forest = rerfClassifier(projection_matrix="S-RerF",
-                                         n_estimators=self.num_trees,
-                                         n_jobs=cpu_count() - 1,
-                                         image_height=length,
-                                         image_width=width,
-                                         patch_height_min=1,
-                                         patch_width_min=1,
-                                         patch_height_max=3,
-                                         patch_width_max=3)
-        self.forest.fit(reshaped_images, labels)
-        #Is this necessary
-        for i in range(length):
-            for j in range(width):
-                MF_image[:, i, j] = np.array([approx_predict_proba_sample_wise(
-                    sample) for sample in images[:, i, j]])[..., np.newaxis]
+            MF_image = self.forest.predict_proba(reshaped_images)
 
         return MF_image
 
@@ -50,13 +53,24 @@ class ConvMF(object):
             raise Exception("Should fit training data before  predicting")
 
         batch_size, length, width, _ = images.shape
+        reshaped_images = images.reshape(batch_size, length*width)
         kernel_predictions = np.zeros((images.shape[0], length, width, 1))
 
-        for i in range(length):
-            for j in range(width):
-                def approx_predict_proba_sample_wise(sample):
-                    return np.array(self.forest.predict_proba(sample.tolist())[1] / float(self.num_trees))
-                kernel_predictions[:, i, j] = np.array([approx_predict_proba_sample_wise(
-                    sample) for sample in images[:, i, j]])[..., np.newaxis]
+        if self.type == 'native':
+            kernel_predictions = self.forest.predict_proba(reshaped_images)
+
+        return kernel_predictions
+
+
+    def final_predict(self, images):
+        if not self.forest:
+            raise Exception("Should fit training data before  predicting")
+
+        batch_size, length, width, _ = images.shape
+        reshaped_images = images.reshape(batch_size, length*width)
+        kernel_predictions = np.zeros((images.shape[0], length, width, 1))
+
+        if self.type == 'native':
+            kernel_predictions = self.forest.predict(reshaped_images)
 
         return kernel_predictions
--- tests/test_conv_mf.py
+++ tests/test_conv_mf.py
@@ -2,20 +2,54 @@ import pytest
 from savanna.utils.dataset import *
 from savanna.inference.conv_mf import ConvMF
 
-def test_empty():
-    """
-    Placeholder
-    """
-    temp = ConvMF()
-    assert True
+def test_convmf_native():
+    numpy_data = dict()
+    (numpy_data["train_images"], numpy_data["train_labels"]), (
+        numpy_data["test_images"],
+        numpy_data["test_labels"],
+    ) = get_dataset("../savanna/data", "FashionMNIST", is_numpy=True)
+    #trainset, testset = get_dataset("../savanna/data", "FashionMNIST", is_numpy=True)
 
-def test_convmf():
-    trainset, testset = get_dataset("../savanna/data", "FashionMNIST", is_numpy=True)
+    trainset, testset = get_subset_data(
+                            dataset_name = "FashionMNIST",
+                            data=numpy_data,
+                            choosen_classes= np.arange(10),
+                            sub_train_indices = np.arange(100)
+                            )
 
-    #trainset, testset = get_subset_data( choosen_classes, sub_train_indices, is_numpy=True, batch_size=None):
+    layer = ConvMF()
+    layerOutput = layer.fit(trainset[0], trainset[1])
 
+    nsamples, nclasses = layerOutput.shape
+    assert nsamples == 100
+    assert nclasses == 10
 
-    test = ConvMF()
-    test.fit(trainset[0], trainset[1])
+    layerOutput = layer.predict(testset[0])
+    nsamples, nclasses = layerOutput.shape
+    assert nsamples == 10000
+    assert nclasses == 10
 
-    assert True
+    results = layer.final_predict(testset[0])
+    count = 0
+    for i in range(len(results)):
+        if results[i] == testset[1][i]:
+            count += 1
+    score = count/nsamples
+
+    assert score > 0.5
+
+    assert hasattr(layer, "type")
+    assert hasattr(layer, "num_trees")
+    assert hasattr(layer, "tree_type")
+    assert hasattr(layer, "patch_height_min")
+    assert hasattr(layer, "patch_height_max")
+    assert hasattr(layer, "patch_width_max")
+    assert hasattr(layer, "patch_width_min")
+
+    assert layer.type == 'native'
+    assert layer.num_trees == 1000
+    assert layer.tree_type == 'S-RerF'
+    assert layer.patch_height_min == 1
+    assert layer.patch_height_max == 5
+    assert layer.patch_width_min == 1
+    assert layer.patch_width_max == 5
